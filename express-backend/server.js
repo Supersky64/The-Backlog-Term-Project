536e3373c9d991edec5ab84eb09a87721148cfc9
Bryan Palma <supersky64ofderps@gmail.com>
2026-05-05 18:41:00 -0400

    fixed mayhaps

--- express-backend/server.js
+++ express-backend/server.js
@@ -23,14 +23,17 @@ app.use(express.static("public"));
 const session = require('express-session');
 const passport = require('passport');
 require('./auth/passport');
+
+app.set('trust proxy', 1);
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly: true,
         secure: true,
-        sameSite: 'none'
+        sameSite: 'none',
+        httpOnly: true
     }
 }));
 app.use(passport.initialize());
